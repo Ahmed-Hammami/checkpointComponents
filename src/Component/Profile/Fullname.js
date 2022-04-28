da8bf5081dd6169950bf409d4e0210b4e56f27df
ahmedhammami <ahammami2016@gmail.com>
2022-04-28 01:14:23 +0100

    frs

---
+++ src/Component/Profile/Fullname.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fullname() {
+  return (
+    <div>
+      <h2>The ocean glow and glitter like the stars in the sky</h2>
+    </div>
+  )
+}
+
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+ import React from 'react'
+ 
+ export default function ProfilePhoto() {
+   return (
+     <div>
+       <img src='/lion.jpg' alt='fff'></img>
+     </div>
+   )
+ }
+ 
